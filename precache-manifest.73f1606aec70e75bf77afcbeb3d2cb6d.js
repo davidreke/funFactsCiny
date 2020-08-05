92941c9e49abc457c11cb5076ff08638ac6c501e
David Reke <david.e.reke@gmail.com>
2020-08-05 15:48:46 -0400

    Updates

---
+++ precache-manifest.73f1606aec70e75bf77afcbeb3d2cb6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa0d7f4d8db0e3b4f187c6114b5b9e7",
+    "url": "/funFactsCiny/index.html"
+  },
+  {
+    "revision": "87b8dda9ee8c91690eb6",
+    "url": "/funFactsCiny/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1a25e4e5ee760defec00",
+    "url": "/funFactsCiny/static/js/2.a5b77128.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/funFactsCiny/static/js/2.a5b77128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b8dda9ee8c91690eb6",
+    "url": "/funFactsCiny/static/js/main.34733fef.chunk.js"
+  },
+  {
+    "revision": "c51ac12972d00d0d5702",
+    "url": "/funFactsCiny/static/js/runtime-main.49c94184.js"
+  }
+]);
